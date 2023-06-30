6358240e547e7282a21c489285991fd416768d41
Agustin Naso <anaso@itba.edu.ar>
2023-06-30 03:23:16 -0300

    feat: player guns animations

--- My project/Assets/Scripts/Strategies/Guns/LaserPistol.cs
+++ My project/Assets/Scripts/Strategies/Guns/LaserPistol.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LaserPistol : Gun
 {
+    Animator animator;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+
+    }
+
 
     public override void Attack()
     {
-        base.Attack();
+        if (CanShoot())
+        {
+            Shoot();
+            audioSource.PlayOneShot(shootingSound);
+            animator.SetTrigger("Shoot");
+        }
+
     }
 
     public override void Reload()
--- My project/Assets/Scripts/Strategies/Guns/Shotgun.cs
+++ My project/Assets/Scripts/Strategies/Guns/Shotgun.cs
@@ -6,17 +6,24 @@ public class Shotgun : Gun
 {
     [SerializeField] private int _bulletsPerShell = 5;
     GameObject _bulletHole;
+    Animator animator;
+
+
 
 
     protected override void Start()
     {
         base.Start();
         //For bullets placement
-        _bulletHole = GameObject.Find("Shotgun_Bullet_Hole"); ;
+        _bulletHole = GameObject.Find("Shotgun_Bullet_Hole");
+        animator = GetComponent<Animator>();
+
     }
 
     protected override void Shoot()
     {
+        Debug.Log("AA");
+        animator.SetTrigger("ShootShotgun");
         currentShotCooldown = ShotCooldown;
         for (int i = 0; i < _bulletsPerShell; i++)
         {
